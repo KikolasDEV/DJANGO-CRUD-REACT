4dc8fe9fcd3d56b8d67dc864f20a0f408b13db32
KikolasDEV <kikoamarillo94@gmail.com>
2024-12-03 15:45:06 +0100

    Crear tareas

--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,5 +1,37 @@
+import { useForm } from "react-hook-form";
+import { createTask } from "../api/tasks.api";
+import { useNavigate } from "react-router-dom";
+
 export function TaskFormPage() {
-    return (
-      <div>Task form page</div>
-    )
-  }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const navigate = useNavigate();
+
+  const onSubmit = handleSubmit(async (data) => {
+    await createTask(data);
+    navigate("/tasks");
+  });
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Title"
+          {...register("title", { required: true })}
+        />
+        {errors.title && <span>title is required</span>}
+        <textarea
+          rows="3"
+          placeholder="Description"
+          {...register("description", { required: true })}
+        ></textarea>
+        {errors.description && <span>description is required</span>}
+        <button>Save</button>
+      </form>
+    </div>
+  );
+}
